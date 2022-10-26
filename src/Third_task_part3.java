8a651f214feafe7ebba32ad4b4a38b23064461ee
moleculoman <Xz2@ZBS@mhSFpg7>
2022-10-26 23:43:44 +0700

    add check to situation, when all numbers are equal

--- src/Third_task_part3.java
+++ src/Third_task_part3.java
@@ -1,7 +1,7 @@
 public class Third_task_part3 {
     public static void main(String[] args) {
         int one = 95;
-        int two = 7;
+        int two = 91;
         int three =95;
         if (one >three && one> two) {
             System.out.println("Число " + one + " бОльшее из всех");
@@ -9,6 +9,8 @@ public class Third_task_part3 {
             System.out.println("Число " + two + " бОльшее из всех");
         } else if (three > one && three > two) {
             System.out.println("Число " + three + " бОльшее из всех");
+        } else if (one == two && two == three && one == three) {
+            System.out.println("Все числа равны");
         } else if (one == two || two== three || one==three) {
             System.out.println("Невозможно определить наибольшее из чисел");
         }
